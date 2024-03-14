bbf23aae8c7608071bc0d10f9e8dc842305ef29c
anna fiochi <annafiochi.tds2.senai@gmail.com>
2024-03-14 11:41:11 -0300

    criei um método

--- array.js
+++ array.js
@@ -1,1 +1,13 @@
-//oi
+let array1 = ['anna', 'giovanna', 'mariana'];
+let array2 = ['sophia', 'leticia', 'thiago'];
+let array3 = array1.concat(array2);
+
+console.log(array3);
+//o esperado é mostrar array ["anna", "giovanna", "mariana","sophia", "leticia", "thiago"]
+
+
+
+
+
+
+
